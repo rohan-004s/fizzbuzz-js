7790be00f7fb0585aa66881ed60267521cad32db
Rohan Sehmar <rohan.sehmar@bp.com>
2022-09-28 16:05:48 +0100

    Fizzbuzz and fizzbang in messy format

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,10 +5,35 @@ function fizzbuzz() {
     
     console.log('Welcome to FizzBuzz!');
 
-    // Put your code here...
 
-    
+
+    for (let i = 1; i<=100; i++) {
+        if (i%3 === 0){
+            if (i%5 == 0){
+                console.log("FizzBuzz");
+            } else if (i%7===0){
+                console.log(i + ": FizzBang")
+            }else{
+                console.log("Fizz");
+            }
+
+        } else if (i%5 === 0){
+            if (i%3 == 0){
+                console.log("FizzBuzz");
+            }else{
+                console.log("Buzz");
+            }
+        } else if (i%7===0){
+            console.log("Bang")
+        }else{
+            console.log(i)
+        }
+    } 
 }
+
+  
+
 
 // Now we run the main function...
 fizzbuzz();
+
